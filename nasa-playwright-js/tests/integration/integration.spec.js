1a72198765b9b05e4e9919f0fd20e41275c794eb
gris04paul <88940837+gris04paul@users.noreply.github.com>
2026-04-01 19:36:40 +0530

    Update integration.spec.js

--- nasa-playwright-js/tests/integration/integration.spec.js
+++ nasa-playwright-js/tests/integration/integration.spec.js
@@ -13,7 +13,7 @@ test('Integration - API to UI consistency', async ({ page, request }) => {
   await nasa.search('moon');
   await page.waitForTimeout(4000);
   await nasa.openFirstResult();
-  //await page.waitForTimeout(40000);
+
 
   const uiTitle = (await page.locator('h1').innerText()).trim().toLowerCase();
   const uiId = await nasa.getNasaIdFromUrl();
@@ -21,21 +21,8 @@ test('Integration - API to UI consistency', async ({ page, request }) => {
    console.log('UI Title:', uiTitle);
     console.log('UI NASA ID:', uiId);
 
-    // ✅ IMPORTANT CHECK
-  //expect(uiId).toBeTruthy();
-
-  // Step 5: Call API using NASA ID
   const assetData = await api.getAsset(uiId);
 
-  // Step 6: Extract API Title
- /* const apiTitle = assetData.collection.items[0].data[0].title
-    .trim()
-    .toLowerCase();*/
-
-  //console.log('API Title:', apiTitle);
-
-  // Step 7: Compare UI vs API
-  //expect(uiTitle).toContain(apiTitle);
-
+ 
 
 });
